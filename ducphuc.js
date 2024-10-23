5f87a6cb81dedd1c9c5f08e90fadbd8740862e3f
depzai556 <vuducphuc31102005@gmail.com>
2024-10-24 01:53:17 +0700

    init project

---
+++ ducphuc.js
@@ -0,0 +1,51 @@
+function toggleMenu(){
+    const menu = document.querySelector('.menu');
+    const nav=document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+
+    bgVideoList.forEach(video=>{
+        video.classList.remove('active');
+        if(video.classList.contains(name)){
+            video.classList.add('active');
+        }
+
+    }
+
+    );
+
+    models.forEach(model=>{
+        model.classList.remove('active');
+        if(model.classList.contains(name)){
+            model.classList.add('active');
+        }
+
+    }
+
+    );
+
+    trailers.forEach(video=>{
+        video.classList.remove('active');
+        if(video.classList.contains(name)){
+            video.classList.add('active');
+        }
+
+    }
+
+    );
+
+
+
+
+
+    
+}
+
+ 
